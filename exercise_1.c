6e3e663c226d13d30032bf13bad8cb8f9a6a7800
timaslj <timaslj@kth.se>
2020-04-20 20:03:55 +0200

    exercise 1 done

--- exercise_1.c
+++ exercise_1.c
@@ -1,13 +1,13 @@
 #include <omp.h>
 #include <stdio.h>
 
-int main() 
-{
+int main() {
 
-#pragma omp parallel
+  #pragma omp parallel
   {
     int ID = omp_get_thread_num();
     printf("Hello World from Thread %d!\n", ID);
   }
 
+  return 0;
 }
